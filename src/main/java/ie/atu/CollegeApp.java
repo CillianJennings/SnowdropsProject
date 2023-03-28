2a1e6c299774222d4c46a0b0554078fac8087a68
Cillian <G00404411@atu.ie>
2023-03-28 14:43:01 +0100

    made changes to classes and added sql database test connectivity

--- src/main/java/ie/atu/CollegeApp.java
+++ src/main/java/ie/atu/CollegeApp.java
@@ -8,7 +8,19 @@ public class CollegeApp {
         Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
-            System.out.print("Enter something: ");
+            System.out.print("Enter student ID: ");
+            String studentID = sc.nextLine();
+        }
+
+        Student s = CollegeDB.getStudent(student_id);
+
+        System.out.println();
+        if (s != null) {
+            System.out.println(s.toString());
+            System.out.println("Student ID: " + s.getStudent_id());
+        }
+        else {
+            System.out.println("No product matches this product code.");
         }
     }
 }
