a2eee79a7ea3f812db7b0aa5bfa45a13e8916b3e
Cillian <G00404411@atu.ie>
2023-04-19 22:30:11 +0100

    testing java database

--- src/main/java/ie/atu/Design1/CollegeApp.java
+++ src/main/java/ie/atu/Design1/CollegeApp.java
@@ -1,6 +1,8 @@
 package ie.atu.Design1;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CollegeApp {
@@ -9,24 +11,24 @@ public class CollegeApp {
 
         Scanner scan = new Scanner(System.in);
         String choice = "y";
-        int operation = 0;
+        int operation;
 
         while (choice.equalsIgnoreCase("y")) {
             System.out.print("What do you want to do:\n1 Select from database\n2 Write to database\n3 Update database\n4 Delete from database");
             operation = scan.nextInt();
+
             switch(operation){
                 case 1:
                     select();
                     break;
                 case 2:
-
-
+                    System.out.println("case 2");
                     break;
                 case 3:
-
+                    System.out.println("case 3");
                     break;
                 case 4:
-
+                    System.out.println("case 4");
                     break;
                 default:
                     System.out.println("Not an option, please enter a number between 1 and 4");
@@ -39,14 +41,18 @@ public class CollegeApp {
     }
 
     public static void select(){
-        String showTables = "SHOW tables";
+        String selectSQL = "SHOW tables";
 
-        try (Connection connection = DriverManager.getConnection(jdbc:mysql://localhost:3306/sakila, root, root);
+        ArrayList<String> tables = new ArrayList<String>();
+
+        try (Connection connection = DatabaseUtils.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(showTables)){
+             ResultSet resultSet = statement.executeQuery(selectSQL)){
 
             while (resultSet.next()) {
-                String course = resultSet.getString();
+                tables.add(resultSet.getString(1));
+
+                System.out.println(Arrays.toString(tables.toArray()));
             }
         } catch (SQLException e) {
             e.printStackTrace();
